b82404e36f41fd0107a66e411eaee79fbe7de3e5
Akkiprakash <akkiprakash741@gmail.com>
2025-12-23 12:57:18 +0530

    model

---
+++ src/main/java/com/model/student.java
@@ -0,0 +1,50 @@
+package com.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "StudentTable")
+public class student {
+    @Id
+    @Column
+    private Integer sid;
+
+    private String sname;
+    private String scity;
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getScity() {
+        return scity;
+    }
+
+    public void setScity(String scity) {
+        this.scity = scity;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" + "sid=" + sid + ", sname='" + sname + '\'' +
+                ", scity='" + scity + '\'' +
+                '}';
+    }
+}
+
